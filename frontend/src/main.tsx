dc4737c3e924cfd799ba1556660fd8fd48457ba1
Adam-Ghanem <2.91402205e+08+Adam-Ghanem@users.noreply.github.com>
2026-08-12 15:58:03 +0000

    design: increase forensic reference fidelity

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./light-theme.css";
 import "./forensic-casefile.css";
 import "./forensic-reference-layout.css";
+import "./forensic-reference-fidelity.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
